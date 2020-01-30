3a30dfe17044528763dd43f1ee935c877aa97994
Ken Gilmer <kgilmer@gmail.com>
2020-01-30 08:12:37 -0800

    Fix deploy script to pass GA tag info.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+env HUGO_ENV="production" hugo
 
 # Go To Public folder
 cd public
